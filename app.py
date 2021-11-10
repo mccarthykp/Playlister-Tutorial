eacfb2473d354f5e72c08be335c1cb93cbf65c11
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-11-10 15:56:55 -0500

    heroku issues

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 
